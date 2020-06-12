4629b08a559bb988eb187712ef9a8c10924981ce
Caio Azevedo <caioba19@gmail.com>
2020-06-12 10:24:39 -0300

    Create project entities

---
+++ TimeMeals/TimeMeals WatchKit Extension/Helpers/Enums/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  TimeMeals WatchKit Extension
+//
+//  Created by Caio Azevedo on 12/06/20.
+//  Copyright © 2020 Caio Azevedo. All rights reserved.
+//
+
+import Foundation
+
+enum MealStatus {
+    case rightTime
+    case wrongTime
+    case notTimeYet 
+}
---
+++ TimeMeals/TimeMeals WatchKit Extension/Model/Report.swift
@@ -0,0 +1,17 @@
+//
+//  Report.swift
+//  TimeMeals WatchKit Extension
+//
+//  Created by Caio Azevedo on 12/06/20.
+//  Copyright © 2020 Caio Azevedo. All rights reserved.
+//
+
+import Foundation
+
+struct Report {
+    var uuid: UUID
+    var weak: Int
+    var totalRightTime: Int
+    var totalWrongTime: Int
+    var mostWrongTimeMeal: String    
+}
